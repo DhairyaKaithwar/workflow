0089bd3117c6d283a02a9198201b0960aa08356d
pinkman. <147752340+DhairyaKaithwar@users.noreply.github.com>
2025-01-07 01:58:39 +0530

    Update FlowExample.jsx

--- src/components/FlowExample.jsx
+++ src/components/FlowExample.jsx
@@ -150,7 +150,7 @@ const FlowExample = ({ onAddNode }) => {
               className="px-2 py-1 bg-green-500 text-white rounded-xl"
               onClick={handleSave}
             >
-              A
+              Apply
             </button>
             <button
               className="px-2 py-1 bg-gray-500 text-white rounded-xl"
